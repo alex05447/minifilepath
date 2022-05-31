ac0bae380ed74780c0631756d2d490c492cb58d4
N/A <N/A>
2022-06-01 02:01:19 +0300

    `file_path_and_name()`

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,7 @@ pub(crate) use {iter::*, util::*};
 pub type FilePathComponent<'a> = &'a ministr::NonEmptyStr;
 
 pub(crate) const SEPARATOR_CHAR: char = '/';
+pub(crate) const SEPARATOR_BYTE: u8 = b'/';
 
 /// Maximum file path component length in bytes (in UTF-8 encoding).
 pub const MAX_COMPONENT_LEN: usize = u8::MAX as usize;
@@ -61,18 +62,18 @@ pub struct FileStemAndExtension<'a> {
 /// Splits the `file_name` into the file stem and extension parts.
 ///
 /// Returns `None` if the `file_name` does not have an extension (or if it somehow ends with a period).
-/// Returns `Some` if the component has an extension and a (maybe empty) file stem.
+/// Returns `Some` if the `file_name` has an extension and a (maybe empty) file stem.
 ///
 /// NOTE: this differs from the standard library w.r.t. path components which start with a period.
-/// Standard library considers a file_name like ".gitignore" to have a file stem part ".gitignore" and no extension.
+/// Standard library considers a file_name like `".gitignore"` to have a file stem part `".gitignore"` and no extension.
 /// This function simply treats anything past the last period as an extension, always.
 ///
 /// E.g.:
-/// - ".txt" -> `Some((None, "txt"))` (NOTE: not `None`)
-/// - "foo.txt" -> `Some((Some("foo"), "txt"))`
-/// - "foo.bar.txt" -> `Some((Some("foo.bar"), "txt"))`
-/// - "foo." -> invalid path (cannot end with a period), but this returns `None`
-/// - "foo" -> `None`
+/// - `".txt"` -> `Some((None, "txt"))` (NOTE: not `None`)
+/// - `"foo.txt"` -> `Some((Some("foo"), "txt"))`
+/// - `"foo.bar.txt"` -> `Some((Some("foo.bar"), "txt"))`
+/// - `"foo."` -> invalid path (cannot end with a period), but this returns `None`
+/// - `"foo"` -> `None`
 pub fn file_stem_and_extension(
     file_name: FilePathComponent<'_>,
 ) -> Option<FileStemAndExtension<'_>> {
@@ -81,9 +82,8 @@ pub fn file_stem_and_extension(
         Some(extension) => extension,
         None => debug_unreachable("`FilePathComponent`'s must be non-empty"),
     };
-    let file_name = iter.next();
 
-    if let Some(file_name) = file_name {
+    if let Some(file_name) = iter.next() {
         // ".txt" -> ("", "txt") -> `Some((None, "txt"))`
         // "foo.txt" -> ("foo", "txt") -> `Some((Some("foo"), "txt"))`
         // "foo.bar.txt" -> ("foo.bar", "txt") -> `Some((Some("foo.bar"), "txt"))`
@@ -102,6 +102,39 @@ pub fn file_stem_and_extension(
     } else {
         None
     }
+}
+
+/// Contains the [`FilePath`]'s file path and file name parts, for [`FilePath`]s which have them.
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub struct FilePathAndName<'a> {
+    /// The file path part of the [`FilePath`].
+    /// E.g. `"foo/bar"` -> `"foo"`, `"foo/bar/baz.txt"` -> `"foo/bar"`.
+    pub file_path: &'a FilePath,
+    /// The file name part of the [`FilePath`].
+    /// E.g. `"foo/bar"` -> `"bar"`, `"foo/bar/baz.txt"` -> `"baz.txt"`.
+    pub file_name: FilePathComponent<'a>,
+}
+
+/// Splits the `file_path` into the file path and file name parts.
+///
+/// E.g. `"foo/bar"` -> `Some{ "foo", "bar" }`, `"foo/bar/baz.txt"` -> `Some{ "foo/bar", "baz.txt" }`, `"bill.cfg"` -> `None`.
+///
+/// Returns `None` if the `file_path` does not have an file path part (i.e. it's just a file name).
+pub fn file_path_and_name(file_path: &FilePathBuf) -> Option<FilePathAndName<'_>> {
+    let mut iter = file_path
+        .as_str()
+        .as_bytes()
+        .rsplitn(2, |b| *b == SEPARATOR_BYTE);
+
+    let file_name = match iter.next() {
+        Some(file_name) => file_name,
+        None => debug_unreachable("`FilePathBuf`'s must be non-empty"),
+    };
+
+    iter.next().map(|file_path| FilePathAndName {
+        file_path: unsafe { FilePath::new_unchecked(str::from_utf8_unchecked(file_path)) },
+        file_name: unsafe { NonEmptyStr::new_unchecked(str::from_utf8_unchecked(file_name)) },
+    })
 }
 
 #[cfg(test)]
@@ -133,5 +166,59 @@ mod tests {
         );
         assert_eq!(file_stem_and_extension(nestr!("foo")), None,);
         assert_eq!(file_stem_and_extension(nestr!("foo.")), None,);
+    }
+
+    #[test]
+    fn file_path_and_name_test() {
+        assert_eq!(file_path_and_name(&FilePathBuf::new(".txt").unwrap()), None);
+        assert_eq!(
+            file_path_and_name(&FilePathBuf::new("foo.txt").unwrap()),
+            None
+        );
+        assert_eq!(
+            file_path_and_name(&FilePathBuf::new("foo.bar.txt").unwrap()),
+            None
+        );
+        assert_eq!(file_path_and_name(&FilePathBuf::new("foo").unwrap()), None);
+        //assert_eq!(file_path_and_name(&FilePathBuf::new("foo.").unwrap()), None); // ComponentEndsWithAPeriod
+
+        assert_eq!(file_path_and_name(&FilePathBuf::new("foo/").unwrap()), None);
+        //assert_eq!(file_path_and_name(&FilePathBuf::new("/foo").unwrap()), None); // RootDirectory
+
+        assert_eq!(
+            file_path_and_name(&FilePathBuf::new("foo/bar").unwrap()),
+            Some(FilePathAndName {
+                file_path: FilePath::new("foo").unwrap(),
+                file_name: nestr!("bar")
+            })
+        );
+        assert_eq!(
+            file_path_and_name(&FilePathBuf::new("foo bar/baz bob").unwrap()),
+            Some(FilePathAndName {
+                file_path: FilePath::new("foo bar").unwrap(),
+                file_name: nestr!("baz bob")
+            })
+        );
+        assert_eq!(
+            file_path_and_name(&FilePathBuf::new("foo/bar/baz bob").unwrap()),
+            Some(FilePathAndName {
+                file_path: FilePath::new("foo/bar").unwrap(),
+                file_name: nestr!("baz bob")
+            })
+        );
+        assert_eq!(
+            file_path_and_name(&FilePathBuf::new("foo/bar.txt").unwrap()),
+            Some(FilePathAndName {
+                file_path: FilePath::new("foo").unwrap(),
+                file_name: nestr!("bar.txt")
+            })
+        );
+        assert_eq!(
+            file_path_and_name(&FilePathBuf::new("foo/.txt").unwrap()),
+            Some(FilePathAndName {
+                file_path: FilePath::new("foo").unwrap(),
+                file_name: nestr!(".txt")
+            })
+        );
     }
 }
